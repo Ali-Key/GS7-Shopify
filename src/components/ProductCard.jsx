1d3753804f631bca70fd6c6a9887b6ff753c1b48
Ali-Key <mafiobio700@gmail.com>
2025-04-22 18:08:46 +0300

    Compelet the project

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
 const ProductCard = ({ product }) => {
-    return (
-      
-      <div className="product-card">
-        <a href={`/product/${product.id}`} className="product-link">
-          <img src={product.image} alt={product.title} className="product-image" />
-        </a>
-        <h3 className="product-title">{product.title}</h3>
-        <div className="product">
-          <p className="product-price">${product.price}</p>
-          <p className="product-category">{product.category}</p>
-        </div>
-        <button className="add-to-cart">Add to Cart</button>
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.title} className="product-image" />
+
+      <h3 className="product-title">{product.title}</h3>
+      <div className="product">
+        <p className="product-price">${product.price}</p>
+        <p className="product-category">{product.category}</p>
       </div>
-    );
-  };
-  
-  export default ProductCard;
+      <div className="product-actions">
+        <button className="add-to-cart">Add to Cart</button>
+        <Link to={`/product/${product.id}`} className="view-details">
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
